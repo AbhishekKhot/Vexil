383959083d8f659a818eda2e8cb74e9a2c10d5d6
abhishekkhot <abhishek.khot@idfy.com>
2026-03-05 11:48:40 +0530

    Project setup

---
+++ services/admin-api/src/entities/Project.ts
@@ -0,0 +1,21 @@
+import { Entity, PrimaryGeneratedColumn, Column, OneToMany, CreateDateColumn } from "typeorm";
+import { Flag } from "./Flag";
+import { Environment } from "./Environment";
+
+@Entity("projects")
+export class Project {
+    @PrimaryGeneratedColumn("uuid")
+    id!: string;
+
+    @Column()
+    name!: string;
+
+    @OneToMany(() => Flag, (flag) => flag.project)
+    flags!: Flag[];
+
+    @OneToMany(() => Environment, (env) => env.project)
+    environments!: Environment[];
+
+    @CreateDateColumn()
+    createdAt!: Date;
+}
